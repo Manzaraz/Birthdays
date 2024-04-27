86d31103c7a55e9dbe37435e82ca2be6b215d165
Christian Manzaraz <magnusmanz@gmail.com>
2024-04-27 12:32:56 -0300

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Christian Manzaraz on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
